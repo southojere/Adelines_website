b2f31c4ec01b1e1ac9cafd90036a970ffb518037
Jeremy Southon <southon55@gmail.com>
2019-02-01 13:49:56 +1300

    added cat on about me hover

--- src/components/leftnav.js
+++ src/components/leftnav.js
@@ -1,20 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import myStyle from '../css/myStyles.css'
 
 class Leftnav extends Component {
-
-    constructor(props){
-        super(props);
+  constructor(props) {
+    super(props)
+    this.handleMouseHover = this.handleMouseHover.bind(this)
+    this.state = {
+      isHovering: false,
     }
+  }
 
-    render() {
-        return (
-            <div className = "nav">
-                 <p className="navItem spacing"><u>About Me</u></p>
-                <p className="navItem"><u>Projects</u></p>
-            </div>
-        );
+  handleMouseHover() {
+    this.setState(this.toggleHoverState)
+  }
+
+  toggleHoverState(state) {
+    return {
+      isHovering: !state.isHovering,
     }
+  }
+
+  render() {
+    let pic = <div></div>
+    if (this.state.isHovering) {
+        console.log('hover');
+        pic = <span>🐱</span>
+    }
+    return (
+      <div className="nav">
+        <p
+          className="navItem spacing"
+          onMouseEnter={this.handleMouseHover}
+          onMouseLeave={this.handleMouseHover}
+        >
+          <u>About Me</u>
+          {pic}
+        </p>
+        <p className="navItem">
+          <u>Projects</u>
+        </p>
+      </div>
+    )
+  }
 }
 
-export default Leftnav;
+export default Leftnav
