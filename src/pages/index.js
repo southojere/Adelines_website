ab74b55f986175d4758c977f41a0112f2e21c4ab
Jeremy Southon <southon55@gmail.com>
2019-06-05 21:02:36 +1200

    back to normal

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,17 +10,39 @@ import Menu from '../components/menu'
 import Hamburger from '../components/hamburger'
 import { view } from 'react-easy-state'
 import Popup from 'reactjs-popup'
-import LandingSection from '../components/landingSection';
 
+const SelectedWorksStyle = {
+  textAlign: 'left',
+  color: '#ffc33e',
+  fontSize: '50px',
+  fontWeight: 'normal',
+  marginBottom: '66px',
+}
 
+const contentStyle = {
+  background: 'rgba(255,255,255,0)',
+  width: '80%',
+  border: 'none',
+}
 
 const images = require.context('../images', true)
 const IndexPage = ({ data }) => (
-  <div>
-    {/* <Layout>
-      <ProjectSection projectFilter={filterStore.filter} store={filterStore} />
-    </Layout> */}
-    <LandingSection></LandingSection></div>
+  <Layout>
+    <Bio />
+    <h5 style={SelectedWorksStyle}>
+      <u>Selected works</u>
+    </h5>
+    <Popup
+      modal
+      overlayStyle={{ background: 'rgba(255,255,255,0.8' }}
+      contentStyle={contentStyle}
+      closeOnDocumentClick={false}
+      trigger={open => <Hamburger open={open} />}
+    >
+      {close => <Menu close={close} store = {filterStore}/>}
+    </Popup>
+    <ProjectSection projectFilter={filterStore.filter} store={filterStore} />
+  </Layout>
 )
 
 export const projectQuery = graphql`
